bf70d7dac20e9298c74ad74557ba80b1bfa137ef
garodrigues <gabriellima7890@gmail.com>
2022-12-26 11:32:56 -0300

    api e validação de usuario

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,23 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import { MdEmail, MdLock } from 'react-icons/md';
+
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
-// import { api } from '../../services/api';
 
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from "yup";
 
+import { api } from '../../services/api';
 import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
 
 const schema = yup.object({
-    email: yup.string().email('email não é valido').required('Campo obrigatório'),
+    email: yup.string().email('Email não é valido').required('Campo obrigatório'),
     password: yup.string().min(3, 'No mínimo 3 caracteres').required('Campo obrigatório'),
 }).required();
 
 const Login = () => {
-
     const navigate = useNavigate();
 
     const {control, handleSubmit, formState: { errors, isValid } } = useForm({
@@ -25,13 +25,18 @@ const Login = () => {
         mode: 'onChange',
     });
 
-    console.log(isValid, errors);
-
-    const onSubmit = data => console.log(data);
-
-    const handleClickSignIn = () => {
-        navigate('/feed')
-    }
+    const onSubmit = async formData => {
+        try{
+            const { data } = await api.get(`users?email=${formData.email}&senha=${formData.password}`);
+            if(data.length === 1){
+                navigate('/feed')
+            } else {
+                alert('Email ou senha inválido')
+            }
+        }catch{
+            alert('Houve um erro, tente novamente')
+        }
+    };
 
     return (<>
         <Header />
