d4ce760d1a5dc136d255b2784253df98bfcf41a6
garodrigues <gabriellima7890@gmail.com>
2023-01-05 12:12:01 -0300

    add register function

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {
   Route,
 } from "react-router-dom";
 import { Feed } from "./pages/feed";
-
+funcionalidade
 import { Home } from './pages/home'
 import { Login } from './pages/login'
 import { Register } from "./pages/register";
